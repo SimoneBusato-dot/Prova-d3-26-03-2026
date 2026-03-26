5acd26aafa79d34bfc7dbbc449f1eaca02f91f75
SimoneBusato-dot <simone.busato@mail.polimi.it>
2026-03-26 11:40:43 +0100

    Primo cerchio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+import * as d3 from 'd3';
+import { onMount } from 'svelte';
+
+let svgElement;
+
+onMount(() => {
+  const svg= d3.select(svgElement);
+  const box = svgElement.getBoundingClientRect()
+  const width= box.width;
+  const height= box.height;
+  const circle = svg.append("circle");
+  circle.attr('r', 200)
+  .attr('cx', width/2)
+  .attr('cy', height/2)
+  
+  
+  
+})
+</script>
+<div class="container">
+ <svg bind:this={svgElement} width="90vw" height="90vh"></svg>
+</div>
+
+<style>
+   .container{
+    width: 90vw;
+    height: 90vh;
+    border: solid 2px grey;
+    border-radius: 30px;
+   }
+</style>
