a551c00a017ff3196d1593057cf65436a318c8af
SimoneBusato-dot <simone.busato@mail.polimi.it>
2026-03-26 14:20:26 +0100

    Adding face import

---
+++ src/lib/components/face.svelte
@@ -0,0 +1,55 @@
+<script>
+import * as d3 from 'd3';
+import { onMount } from 'svelte';
+    import { transform } from 'typescript';
+
+let svgElement;
+
+onMount(() => {
+  const svg= d3.select(svgElement);
+  const box = svgElement.getBoundingClientRect()
+  const width= box.width;
+  const height= box.height;
+  const eyeSpaing = 50;
+  const eyeOffste = 70;
+  const arcGenerator = d3.arc()
+  .innerRadius(80)
+  .outerRadius(100)
+  .startAngle(Math.PI/2)
+  .endAngle(Math.PI*3/2);
+  
+  const circle = svg.append("circle")
+  .attr('r', 200)
+  .attr('cx', width/2)
+  .attr('cy', height/2)
+  .attr('fill', 'yellow')
+  .attr('stroke', 'black')
+
+  const lftEye = svg.append("circle")
+  .attr('r', 20)
+  .attr('cx', width/2 - eyeSpaing)
+  .attr('cy', height/2 - eyeOffste)
+  .attr('fill', 'black')
+  .attr('stroke', 'black')
+  .transition().duration(2000)
+  .attr('cy', height/2 - eyeOffste -40)
+  .transition().duration(2000)
+  .attr('cy', height/2 - eyeOffste)
+  
+  const rgtEye = svg.append("circle")
+  .attr('r', 20)
+  .attr('cx', width/2 + eyeSpaing)
+  .attr('cy', height/2 - eyeOffste)
+  .attr('fill', 'black')
+  .attr('stroke', 'black')
+
+  const smile = svg.append('path')
+  .attr('d', arcGenerator)
+  .attr('transform', `translate(${width/2}, ${height/2})`)
+  
+  
+  
+})
+</script>
+
+<svg bind:this={svgElement} width="90vw" height="90vh"></svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,10 @@
 <script>
-import * as d3 from 'd3';
-import { onMount } from 'svelte';
-
-let svgElement;
-
-onMount(() => {
-  const svg= d3.select(svgElement);
-  const box = svgElement.getBoundingClientRect()
-  const width= box.width;
-  const height= box.height;
-  const circle = svg.append("circle");
-  circle.attr('r', 200)
-  .attr('cx', width/2)
-  .attr('cy', height/2)
-  
-  
-  
-})
+    import Face from "$lib/components/face.svelte";
 </script>
+
+
 <div class="container">
- <svg bind:this={svgElement} width="90vw" height="90vh"></svg>
+  <Face />
 </div>
 
 <style>
